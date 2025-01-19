5b7f9cffa3b74ac3346630e2f016d2685dc1fa42
Spokay <hugobressange@gmail.com>
2025-01-19 23:41:03 +0100

    renaming the wildlens_model_service.py in prediction_service.py, implemented the method to check the footprint with the binary classification model and the method to classify an image to a specie prediction

---
+++ app/services/prediction_service.py
@@ -0,0 +1,67 @@
+from PIL import Image
+from fastapi import UploadFile, HTTPException
+import numpy as np
+
+from app.classifier_models import binary_classifier_model, multiclass_classifier_model
+from app.config import WILDLENS_FOOTPRINT_BINARY_CLASSIFICATION_THRESHOLD
+
+
+def assert_content_type_is_valid(content_type: str):
+    if content_type not in ["image/jpeg", "image/png"]:
+        raise HTTPException(status_code=400, detail="Invalid file type. Please upload a JPEG or PNG image.")
+
+
+class PredictionService:
+    def __init__(self):
+        self.binary_classifier_model = binary_classifier_model
+        self.multiclass_classifier_model = multiclass_classifier_model
+
+
+    def check_image_for_footprint(self, image_file: UploadFile) -> bool:
+        try:
+            assert_content_type_is_valid(image_file.content_type)
+
+            image = Image.open(image_file.file).convert("RGB")
+
+            image_array = np.array(image)
+
+            input_tensor = np.expand_dims(image_array, axis=0)
+
+            # Perform inference (preprocessing is handled in the model)
+            predictions = self.binary_classifier_model.predict(input_tensor)
+
+            # Return True if the model predicts a footprint, False otherwise
+            return predictions[0][0] >= WILDLENS_FOOTPRINT_BINARY_CLASSIFICATION_THRESHOLD
+
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=f"Error while processing footprint check: {str(e)}")
+
+    def classify_image(self, image_file: UploadFile) -> dict:
+        try:
+            assert_content_type_is_valid(image_file.content_type)
+
+            image = Image.open(image_file.file).convert("RGB")
+
+            image_array = np.array(image)
+
+            input_tensor = np.expand_dims(image_array, axis=0)
+
+            # Perform inference (preprocessing is handled in the model)
+            predictions = self.multiclass_classifier_model.predict(input_tensor)
+
+            # Get the class with the highest probability
+            predicted_class = np.argmax(predictions[0])
+
+            # Get the probability of the predicted class
+            predicted_probability = predictions[0][predicted_class]
+
+            return {
+                "class": predicted_class,
+                "probability": predicted_probability
+            }
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=f"Error while processing footprint classification: {str(e)}")
+
+
+
+prediction_service = PredictionService()
