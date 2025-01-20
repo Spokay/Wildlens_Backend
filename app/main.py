7ebd76b1e4268f0b78f3387b619acc1946408ddb
Spokay <hugobressange@gmail.com>
2025-01-20 21:22:05 +0100

    added context manager to manage the lifecycle of the application, new methods to load the ai models at startup. SQLAlchemy setup finished, SQLModels started for the species

--- app/main.py
+++ app/main.py
@@ -1,9 +1,41 @@
-from fastapi import FastAPI
+from contextlib import asynccontextmanager
 
+from fastapi import FastAPI, Depends
+from sqlalchemy.sql.annotation import Annotated
+from sqlmodel import SQLModel, Session
+
+from app.classifier_models import load_models
+from app.config import database_engine
 from .routes import users, ai_model
 
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(database_engine)
+
+
+def get_session():
+    with Session(database_engine) as session:
+        yield session
+
+
+SessionDep = Annotated[Session, Depends(get_session)]
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Executed before startup (setup):
+    load_models()
+    create_db_and_tables()
+    # ------------------------------
+    yield # <--- This is where the context manager pauses and the application starts
+    # ------------------------------
+    # Executed after shutdown (cleanup):
+    #
+    #
+
 app = FastAPI(
-    root_path="/api"
+    root_path="/api",
+    lifespan=lifespan
 )
 
 app.include_router(users.router)
