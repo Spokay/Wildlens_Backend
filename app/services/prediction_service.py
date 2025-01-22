ab89b506db1a3076e796ebaf8c7e0d5323adf6ae
Spokay <hugobressange@gmail.com>
2025-01-22 23:33:23 +0100

    refactoring the prediction service duplicated code and fix the return types of the model predictions

--- app/services/prediction_service.py
+++ app/services/prediction_service.py
@@ -1,69 +1,68 @@
+from typing import Any
+
 from PIL import Image
 from fastapi import UploadFile, HTTPException
 import numpy as np
+from numpy import ndarray, dtype, generic
 
 from app.classifier_models import binary_classifier_model, multiclass_classifier_model
 from app.config import WILDLENS_FOOTPRINT_BINARY_CLASSIFICATION_THRESHOLD
 from app.models.specie import SpeciePrediction
 
 
+def prepare_input_tensor(image_file: UploadFile) -> ndarray[Any, dtype[generic | Any]]:
+    image = Image.open(image_file.file).convert("RGB")
+
+    image_array = np.array(image)
+
+    return np.expand_dims(image_array, axis=0)
+
+
 class PredictionService:
-    def __init__(self, binary_classifier, multiclass_classifier):
+    def __init__(self, binary_classifier, multiclass_classifier, nb_classes=10):
         self.binary_classifier_model = binary_classifier
         self.multiclass_classifier_model = multiclass_classifier
+        self.nb_classes = nb_classes
+        self.classes = list(range(nb_classes))
 
 
     def check_image_for_footprint(self, image_file: UploadFile) -> bool:
         try:
-            image = Image.open(image_file.file).convert("RGB")
-
-            image_array = np.array(image)
-
-            input_tensor = np.expand_dims(image_array, axis=0)
-
-            print(input_tensor.shape)
+            input_tensor = prepare_input_tensor(image_file)
 
             # Perform inference (preprocessing is handled in the model)
             predictions = self.binary_classifier_model.predict(input_tensor)
 
-            print(predictions)
+            probability = predictions[0]
 
-            # Return True if the model predicts a footprint, False otherwise
-            return predictions[0][0] >= WILDLENS_FOOTPRINT_BINARY_CLASSIFICATION_THRESHOLD
+            # Return True if the probability that it is a footprint is greater than the threshold, False otherwise
+            return bool(probability >= WILDLENS_FOOTPRINT_BINARY_CLASSIFICATION_THRESHOLD)
 
         except Exception as e:
             raise HTTPException(status_code=500, detail=f"Error while processing footprint check: {str(e)}")
 
     def classify_image(self, image_file: UploadFile) -> list[SpeciePrediction]:
         try:
-            image = Image.open(image_file.file).convert("RGB")
-
-            image_array = np.array(image)
-
-            input_tensor = np.expand_dims(image_array, axis=0)
-
-            print(input_tensor.shape)
+            input_tensor = prepare_input_tensor(image_file)
 
             # Perform inference (preprocessing is handled in the model)
             predictions = self.multiclass_classifier_model.predict(input_tensor)
 
-            print(predictions)
 
             # Get the top 3 predicted classes with their probabilities
-            top_classes = np.argsort(predictions[0])[-3:][::-1]
-
-            top_probabilities = predictions[0][top_classes]
+            probability_by_classes = zip(predictions, self.classes)
+            top_3_predictions = sorted(probability_by_classes, key=lambda x: x[0], reverse=True)[:3]
 
             return [
-                SpeciePrediction(class_number=class_number, probability=probability) for class_number, probability in zip(top_classes, top_probabilities)
+                SpeciePrediction(class_number=class_number, probability=probability) for probability, class_number  in top_3_predictions
             ]
 
         except Exception as e:
             raise HTTPException(status_code=500, detail=f"Error while processing footprint classification: {str(e)}")
 
 
-
 prediction_service = PredictionService(
     binary_classifier=binary_classifier_model,
-    multiclass_classifier=multiclass_classifier_model
+    multiclass_classifier=multiclass_classifier_model,
+    nb_classes=10
 )
