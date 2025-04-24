f0ca4b148ca8c6a9b9cb305e6a9c49460c705e92
OCE4Ned <mel.delescluse@gmail.com>
2025-04-24 21:11:26 +0200

    current: get species identified by current user

--- app/database.py
+++ app/database.py
@@ -3,29 +3,18 @@ import os
 from sqlalchemy import create_engine
 from sqlmodel import SQLModel, Session
 
-from app.models import (
-    Role,
-    Family,
-    Habitat,
-    Specie,
-    User,
-    Badge,
-    UserBadge,
-    SpecieHabitat,
-    Identification,
-    BadgeCriteria,
-)
+from app.models import Role, Family, Habitat, Specie, User, Badge, UserBadge, SpecieHabitat, Identification, \
+    BadgeCriteria
 
-host = os.getenv("DB_HOST", "localhost")
-port = os.getenv("DB_PORT", 3306)
-database = os.getenv("DB_NAME", "")
-user = os.getenv("DB_USER", "")
-password = os.getenv("DB_PASSWORD", "")
+host = os.getenv('DB_HOST', 'localhost')
+port = os.getenv('DB_PORT', 3306)
+database = os.getenv('DB_NAME', '')
+user = os.getenv('DB_USER', '')
+password = os.getenv('DB_PASSWORD', '')
 
 conn_string = f"mariadb+pymysql://{user}:{password}@{host}:{port}/{database}"
 
 database_engine = create_engine(conn_string)
-
 
 def get_session():
     with Session(database_engine) as session:
@@ -35,23 +24,20 @@ def get_session():
             session.close()
 
 
+
 def create_db_and_tables(engine):
-    SQLModel.metadata.create_all(
-        engine,
-        tables=[
-            Role.__table__,
-            Family.__table__,
-            Habitat.__table__,
-            Specie.__table__,
-            User.__table__,
-            Badge.__table__,
-            UserBadge.__table__,
-            SpecieHabitat.__table__,
-            Identification.__table__,
-            BadgeCriteria.__table__,
-        ],
-        checkfirst=True,
-    )
+    SQLModel.metadata.create_all(engine, tables=[
+        Role.__table__,
+        Family.__table__,
+        Habitat.__table__,
+        Specie.__table__,
+        User.__table__,
+        Badge.__table__,
+        UserBadge.__table__,
+        SpecieHabitat.__table__,
+        Identification.__table__,
+        BadgeCriteria.__table__
+    ], checkfirst=True)
 
     # add default roles
     with Session(engine) as session:
@@ -64,83 +50,106 @@ def create_db_and_tables(engine):
         session.refresh(role2)
         session.close()
 
-    # session = Session(engine)
-    #
-    # criteria1 = {
-    #     "type": "identification_count_by_specie",
-    #     "required": 5
-    # }
-    # criteria2 = {
-    #     "type": "identification_count_by_specie",
-    #     "required": 10
-    # }
-    #
-    # criteria3 = {
-    #     "type": "identification_count_by_specie",
-    #     "required": 5,
-    #     "specie": 1
-    # }
-    # criteria4 = {
-    #     "type": "identification_count_by_specie",
-    #     "required": 10,
-    #     "specie": 1
-    # }
-    #
-    # badge = Badge(
-    #     name="Explorateur de la nature",
-    #     description="Décerné pour avoir identifié 5 espèces différentes"
-    # )
-    # badge2 = Badge(
-    #     name="Véteran de la nature",
-    #     description="Décerné pour avoir identifié 10 espèces différentes"
-    # )
-    #
-    # badge3 = Badge(
-    #     name="Amateur de chat",
-    #     description="Décerné pour avoir identifié 5 chats"
-    # )
-    #
-    # badge4 = Badge(
-    #     name="Expert en chat",
-    #     description="Décerné pour avoir identifié 10 chats"
-    # )
-    # session.add(badge)
-    # session.add(badge2)
-    # session.add(badge3)
-    # session.add(badge4)
-    # session.commit()
-    # session.refresh(badge)
-    # session.refresh(badge2)
-    # session.refresh(badge3)
-    # session.refresh(badge4)
-    #
-    # badge_criteria = BadgeCriteria(
-    #     badge_id=badge.id,
-    #     criteria=criteria1
-    # )
-    #
-    # badge_criteria2 = BadgeCriteria(
-    #     badge_id=badge2.id,
-    #     criteria=criteria2
-    # )
-    #
-    # badge_criteria3 = BadgeCriteria(
-    #     badge_id=badge3.id,
-    #     criteria=criteria3
-    # )
-    #
-    # badge_criteria4 = BadgeCriteria(
-    #     badge_id=badge4.id,
-    #     criteria=criteria4
-    # )
-    #
-    # session.add(badge_criteria)
-    # session.add(badge_criteria2)
-    # session.add(badge_criteria3)
-    # session.add(badge_criteria4)
-    # session.commit()
-    # session.refresh(badge_criteria)
-    # session.refresh(badge_criteria2)
-    # session.refresh(badge_criteria3)
-    # session.refresh(badge_criteria4)
-    # session.close()
+    session = Session(engine)
+
+    criteria1 = {
+        "type": "identification_count_by_specie",
+        "required": 5
+    }
+    criteria2 = {
+        "type": "identification_count_by_specie",
+        "required": 10
+    }
+
+    criteria3 = {
+        "type": "identification_count_by_specie",
+        "required": 5,
+        "specie": 1
+    }
+    criteria4 = {
+        "type": "identification_count_by_specie",
+        "required": 10,
+        "specie": 1
+    }
+
+    badge = Badge(
+        name="Explorateur de la nature",
+        description="Décerné pour avoir identifié 5 espèces différentes"
+    )
+    badge2 = Badge(
+        name="Véteran de la nature",
+        description="Décerné pour avoir identifié 10 espèces différentes"
+    )
+
+    badge3 = Badge(
+        name="Amateur de chat",
+        description="Décerné pour avoir identifié 5 chats"
+    )
+
+    badge4 = Badge(
+        name="Expert en chat",
+        description="Décerné pour avoir identifié 10 chats"
+    )
+    session.add(badge)
+    session.add(badge2)
+    session.add(badge3)
+    session.add(badge4)
+    session.commit()
+    session.refresh(badge)
+    session.refresh(badge2)
+    session.refresh(badge3)
+    session.refresh(badge4)
+
+    badge_criteria = BadgeCriteria(
+        badge_id=badge.id,
+        criteria=criteria1
+    )
+
+    badge_criteria2 = BadgeCriteria(
+        badge_id=badge2.id,
+        criteria=criteria2
+    )
+
+    badge_criteria3 = BadgeCriteria(
+        badge_id=badge3.id,
+        criteria=criteria3
+    )
+
+    badge_criteria4 = BadgeCriteria(
+        badge_id=badge4.id,
+        criteria=criteria4
+    )
+
+    session.add(badge_criteria)
+    session.add(badge_criteria2)
+    session.add(badge_criteria3)
+    session.add(badge_criteria4)
+    session.commit()
+    session.refresh(badge_criteria)
+    session.refresh(badge_criteria2)
+    session.refresh(badge_criteria3)
+    session.refresh(badge_criteria4)
+    session.close()
+
+    family1 = Family(
+        name="Mammifères",
+    )
+
+    specie1 = Specie(
+        name="Chat",
+        latin_name="Felis catus",
+        description="Le chat domestique est un mammifère carnivore de la famille des félidés.",
+        size="Petit",
+        region="Domestique",
+        fun_fact="Les chats peuvent faire des bonds jusqu'à six fois leur taille.",
+        specie_exemple_photo="https://example.com/chat.jpg",
+        footprint_exemple_photo="https://example.com/chat_pattes.jpg",
+        family_id=1
+    )
+
+    session.add(family1)
+    session.add(specie1)
+    session.commit()
+    session.refresh(family1)
+    session.refresh(specie1)
+    session.close()
--- app/mappers/specie_mapper.py
+++ app/mappers/specie_mapper.py
@@ -64,16 +64,40 @@ class SpecieMapper:
             habitats=habitats_response,
         )
 
+    async def specie_to_complete_info_response(self, specie: Specie) -> SpecieResponse:
+        family_response = await self.family_mapper.family_to_response(specie.family)
+        habitats_response = [await self.habitat_mapper.habitat_to_response(habitat) for habitat in specie.habitats]
+        print(specie)
+
+        return SpecieResponse(
+            id=specie.id,
+            name=specie.name,
+            latin_name=specie.latin_name,
+            description=specie.description,
+            size=specie.size,
+            region=specie.region,
+            fun_fact=specie.fun_fact,
+            specie_exemple_photo_url=specie.specie_exemple_photo,
+            footprint_exemple_photo_url=specie.footprint_exemple_photo,
+            family=family_response,
+            habitats=habitats_response,
+            #identification_date=identification_response
+        )
+
     async def species_to_basic_info_responses(
         self, species: list[Specie]
     ) -> list[SpecieBasicInfoResponse]:
         if not species:
             return []
-
         return [await self.specie_to_basic_info_response(specie) for specie in species]
 
     async def species_to_responses(self, species: list[Specie]) -> list[SpecieResponse]:
         return [await self.specie_to_response(specie) for specie in species]
+
+    async def species_to_complete_info_responses(self, species: list[Specie]) -> list[SpecieResponse]:
+        return [
+            await self.specie_to_complete_info_response(specie) for specie in species
+        ]
 
     async def specie_to_prediction_response(
         self, specie: Specie, probability: float
