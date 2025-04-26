463709f429b754203904ad504a2527db63ba2b24
Fod9 <dernoncourt.tom@gmail.com>
2025-04-26 16:34:01 +0200

    CRUD ok for families

--- app/dto/family.py
+++ app/dto/family.py
@@ -1,6 +1,16 @@
+from pydantic import BaseModel
 from sqlmodel import SQLModel
+from typing import Optional
 
 
 class FamilyResponse(SQLModel):
     id: int
     name: str
+
+
+class CreateFamilyInfo(BaseModel):
+    name: str
+
+
+class UpdateFamilyInfo(BaseModel):
+    name: Optional[str] = None
--- app/main.py
+++ app/main.py
@@ -14,9 +14,12 @@ from prometheus_fastapi_instrumentator import Instrumentator
 from fastapi import FastAPI, Depends
 from sqlmodel import Session
 
-from app.services.authentication_service import AuthMiddleware, ExceptionHandlerLoggingMiddleware
+from app.services.authentication_service import (
+    AuthMiddleware,
+    ExceptionHandlerLoggingMiddleware,
+)
 from app.database import create_db_and_tables, get_session, database_engine
-from app.routes import users, species
+from app.routes import users, species, families
 
 SessionDep = Annotated[Session, Depends(get_session)]
 
@@ -33,10 +36,7 @@ async def lifespan(app_object: FastAPI):
     #
 
 
-app = FastAPI(
-    root_path=API_PREFIX,
-    lifespan=lifespan
-)
+app = FastAPI(root_path=API_PREFIX, lifespan=lifespan)
 
 Instrumentator().instrument(app).expose(app)
 
@@ -45,6 +45,7 @@ app.add_middleware(ExceptionHandlerLoggingMiddleware)
 
 app.include_router(users.router)
 app.include_router(species.router)
+app.include_router(families.router)
 
 if __name__ == "__main__":
     port = int(os.getenv("APP_PORT", 8002))
