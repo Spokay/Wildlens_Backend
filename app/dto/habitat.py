34d7f681ba4f84b10a1e79d37945fffc148af9e1
Spokay <hugobressange@gmail.com>
2025-05-12 15:22:43 +0200

    rebasing 6.3 on master

--- app/dto/habitat.py
+++ app/dto/habitat.py
@@ -1,7 +1,18 @@
 from sqlmodel import SQLModel
+from pydantic import BaseModel
 
 
 class HabitatResponse(SQLModel):
     id: int
     name: str
     description: str | None
+
+
+class CreateHabitatInfo(BaseModel):
+    name: str
+    description: str
+
+
+class UpdateHabitatInfo(BaseModel):
+    name: str
+    description: str
